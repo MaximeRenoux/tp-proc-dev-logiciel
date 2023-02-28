fecc0739f3d788234c362d392a07b27d36cc304c
MaximeRenoux <renoux.maxime@ntymail.com>
2023-02-28 09:57:10 +0100

    application de base marche

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Bonjour")
+
+window.addEventListener("load", () => {
+
+    console.log("hello from js")
+
+    document.getElementById("champ_poids").addEventListener("change", calculIMC); 
+    document.getElementById("champ_taille").addEventListener("change", calculIMC); 
+
+    function calculIMC(){
+
+        poids = document.getElementById("champ_poids").value;
+        taille = document.getElementById("champ_taille").value;
+
+        imc = poids/(taille^2)
+
+        result = document.getElementById("mainSpan");
+
+        result.innerHTML = imc
+        console.log(imc)
+    
+    }
+
+})
+
+
