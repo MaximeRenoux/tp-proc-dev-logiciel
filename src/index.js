dbffc57d251615b4cab7d0f9866bfd3e805c41a2
MaximeRenoux <renoux.maxime@ntymail.com>
2023-02-28 10:19:08 +0100

    ajout boutton

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ window.addEventListener("load", () => {
 
     console.log("hello from js")
 
-    document.getElementById("champ_poids").addEventListener("change", calculIMC); 
-    document.getElementById("champ_taille").addEventListener("change", calculIMC); 
+    // document.getElementById("champ_poids").addEventListener("change", calculIMC); 
+    // document.getElementById("champ_taille").addEventListener("change", calculIMC);
+    document.getElementById("boutton_calcul").addEventListener("click", calculIMC);
+
 
     function calculIMC(){
 
         const poids = document.getElementById("champ_poids").value;
-        const taille = parseFloat(document.getElementById("champ_taille").value);
+        const taille = parseFloat(document.getElementById("champ_taille").value)/100;
 
         const imc = poids/(taille*taille)
 
