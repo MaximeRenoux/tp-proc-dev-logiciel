14ae2121484d9a77789e7daa02ca505488676922
ESSABRI Abdelhadi <abdelhadi.essabri@alumni.univ-avignon.fr>
2023-02-28 09:59:02 +0100

    page html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Bonjour")
+function calculerIMC() {
+  var poids = document.getElementById("poidsInput").value;
+  var taille = document.getElementById("tailleInput").value;
+  var resultatSpan = document.getElementById("resultat");
+
+  if (poids === "" || taille === "") {
+    resultatSpan.innerHTML = "Veuillez entrer votre poids et votre taille.";
+  } else {
+    var imc = poids / ((taille / 100) * (taille / 100));
+    resultatSpan.innerHTML = imc.toFixed(2);
+  }
+}
