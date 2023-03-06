4b784aaba3f5a7d0a0b4d6082324e2a13883b02c
jbravocastri <jenny-paola.bravo@alumni.univ-avignon.fr>
2023-03-06 16:37:52 +0100

    tester message erreur

--- src/index.js
+++ src/index.js
@@ -9,19 +9,38 @@ window.addEventListener("load", () => {
 
 
     function calculIMC(){
-
-        const poids = document.getElementById("champ_poids").value;
-        const taille = parseFloat(document.getElementById("champ_taille").value)/100;
-
-        const imc = poids/(taille*taille)
-
+        //jenny
+        //const monPoids = document.querySelector('var_poids').value;
+        const poids = document.getElementById('champ_poids').value;
+        //const maTaille = parseFloat(document.getElementById('var_taille').value)/100;
+        const taille = document.getElementById('champ_taille').value/100;
+        const imc = poids/(taille*taille);
         const result = document.getElementById("mainSpan");
+        result.innerHTML = imc;
+        console.log(imc.toPrecision(3));
 
-        result.innerHTML = imc
-        console.log(imc)
-    
+        // Quita la validación mientras escribes
+        //poids.addEventListener('champ_poids', () => {
+        // Quita el mensaje según escribes
+        //poids.setCustomValidity('');
+        // Comprueba si debe validarlo
+        //poids.checkValidity();
+        //});
+
+        // Muestra el mensaje de validación
+        //poids.addEventListener('invalid', () => {
+        //poids.setCustomValidity('No es un número');
+        //});
+        //jenny
+
+        //const poids = document.getElementById("champ_poids").value;
+        //const taille = parseFloat(document.getElementById("champ_taille").value)/100;
+        //const imc = poids/(taille*taille)
+        //const result = document.getElementById("mainSpan");
+        //result.innerHTML = imc
+        //console.log(imc)
+
     }
 
 })
-
 
