b729931a9cee10ece41b068249c311c6b175e5e4
djodjo89 <mathysimion@yahoo.fr>
2023-03-27 20:33:27 +0200

    securiser les valeurs du formulaire en fonction des checkbox

--- src/index.js
+++ src/index.js
@@ -133,27 +133,30 @@ window.addEventListener("load", () => {
     
     }
 
-    function afficherResultat(){
+    function afficherResultat(calculerIMC = true, calculerMBasal = true){
         const result = document.getElementById("mainSpan");
         let html = ""
         let afffichage_imc = ''
         let affichage_meta_basal = ''
         let affichage_objectif = ''
+	const afficherIMC = document.getElementById("check_imc").checked == true && calculerIMC
+	const afficherMBasal = calculerMBasal && document.getElementById("champ_mbasal").checked == true
 
-        if (document.getElementById("check_imc").checked == true){
+        if (afficherIMC){
             imc = calculIMC()
             afffichage_imc = afffichage_imc.concat("<span>", "IMC : ", imc.toString(), "</span>")
+
+        	if (afficherMBasal){
+            		meta_basal = calculMetabolismeBasal()
+            		metal_basal_kcal = meta_basal*239
+            		affichage_meta_basal = affichage_meta_basal.concat("<span>", "Métabolisme Basal : ", metal_basal_kcal, "KCal ou ", meta_basal, "MJ", "</span>")
+
+            		if(document.getElementById("champ_objectif").value != "0"){
+                		affichage_objectif = affichage_objectif.concat(afficher_calories)
+            		}
+        	}
         }
 
-        if (document.getElementById("champ_mbasal").checked == true){
-            meta_basal = calculMetabolismeBasal()
-            metal_basal_kcal = meta_basal*239
-            affichage_meta_basal = affichage_meta_basal.concat("<span>", "Métabolisme Basal : ", metal_basal_kcal, "KCal ou ", meta_basal, "MJ", "</span>")
-
-            if(document.getElementById("champ_objectif").value != "0"){
-                affichage_objectif = affichage_objectif.concat(afficher_calories)
-            }
-        }
 
         html = html.concat(afffichage_imc, affichage_meta_basal, affichage_objectif)
         result.innerHTML = html
@@ -169,6 +172,21 @@ window.addEventListener("load", () => {
         return Math.round(metabolismeBasalEnJoules)
     }
 
+	const valeurNumCorrecte = (nomChamp, valeur) => {
+		const { min, max } = document.getElementById(nomChamp)
+		const valeurFloat = parseFloat(valeur)
+
+		return valeurFloat >= parseFloat(min) && valeurFloat <= parseFloat(max)
+	}
+	const valeurSelectPossible = (nomChamp, valeur) => {
+		const aRemplir = document.getElementById(nomChamp)
+		try {
+			const options= [...aRemplir.options].map(option => option.value)
+			return options.includes(valeur)
+		} catch(e){
+			return false
+		}
+	}
 	const CHAMPS = ['mbasal', 'poids', 'taille', 'sexe', 'objectif', 'age']
 	function remplirChampsAvecValeursUrl() {
 		const chaineRequete = window.location.search
@@ -213,9 +231,34 @@ window.addEventListener("load", () => {
 				console.warn(`le paramètre "${cle}" n’a pas de champ correspondant`)
 			}
 		})
-        if (champs.length > 0){
+	const cles = champs.map(([cle, _]) => cle)
+	const demandeCalculMBasal = JSON.parse(document.getElementById(`champ_mbasal`).checked)
+	const calculIMCPossible = cles.includes('taille') && cles.includes('poids')
+	const calculMBasalPossible = demandeCalculMBasal && calculIMCPossible && cles.includes('age') && cles.includes('sexe') && cles.includes('objectif')
+	if (calculIMCPossible) {
+		const [_, taille] = champs.find(([cle, _]) => cle == 'taille')
+		const [__, poids] = champs.find(([cle, _]) => cle == 'poids')
+		const tailleCorrect = valeurNumCorrecte('champ_taille', taille)
+		const poidsCorrect = valeurNumCorrecte('champ_poids', poids)
+		if (tailleCorrect && poidsCorrect) {
+			if (calculMBasalPossible) {
+				const [___, age] = champs.find(([cle, _]) => cle == 'age')
+				const [____, sexe] = champs.find(([cle, _]) => cle == 'sexe')
+				const [_____, objectif] = champs.find(([cle, _]) => cle == 'objectif')
+				const ageCorrect = valeurNumCorrecte('champ_age', age)
+				const sexeCorrect = valeurSelectPossible('champ_sexe', sexe)
+				const objectifCorrect = valeurSelectPossible('champ_objectif', objectif)
+				if (ageCorrect && sexeCorrect && objectifCorrect) {
+            				calculMetabolismeBasal()
+            				afficherResultat(true, true)
+				} else afficherResultat(true, false)
+			} else afficherResultat(true, false)
+            		sauvegarderHistorique()
+		}
+	}
+	else if (champs.length > 0){
             calculMetabolismeBasal()
-            afficherResultat()
+            afficherResultat(false)
             sauvegarderHistorique()
         }
 	}
